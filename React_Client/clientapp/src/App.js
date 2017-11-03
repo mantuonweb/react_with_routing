b6e2d0d66ae4dec71a802cc7cfc74837af08ef04
mantuonweb <mantu.nigam@globallogic.com>
2017-11-03 16:06:20 +0530

    added routing logic

--- React_Client/clientapp/src/App.js
+++ React_Client/clientapp/src/App.js
@@ -1,19 +1,94 @@
 import React, { Component } from 'react';
+//https://reacttraining.com/react-router/web/example/basic
+//https://github.com/ReactTraining/react-router/tree/master/packages/react-router-dom
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+class Home extends Component {
+  render() {
+    return (
+       <div>
+        <h2>Home</h2>
+      </div>
+    );
+  }
+}
+class About extends Component {
+  render() {
+    return (
+       <div>
+        <h2>About</h2>
+       </div>
+    );
+  }
+}
+class Topics extends Component {
+  constructor({match}){
+    console.log(match);
+    super();
+    this.match=match;
+  }
+  render() {
+    return (
+      <div>
+          <h2>Topics</h2>
+          <ul>
+            <li>
+              <Link to={`${this.match.url}/rendering`}>
+                Rendering with React
+              </Link>
+            </li>
+            <li>
+              <Link to={`${this.match.url}/components`}>
+                Components
+              </Link>
+            </li>
+            <li>
+              <Link to={`${this.match.url}/props-v-state`}>
+                Props v. State
+              </Link>
+            </li>
+          </ul>
+
+          <Route path={`${this.match.url}/:topicId`} component={Topic}/>
+          <Route exact path={this.match.url} render={() => (
+            <h3>Please select a topic.</h3>
+          )}/>
+    </div>
+    );
+  }
+}
+
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+   return (
+       <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/topics" component={Topics}/>
+        </div>
+      </Router>
     );
   }
 }
